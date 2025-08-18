5b38c46a2a00bfa63722cc59da59eb5a005e7372
daud1436 <daudeuro@gmail.com>
2025-08-18 03:11:14 +0200

    fixes Part 1 exercise 1.2

--- Part_1/courseinfo/src/App.jsx
+++ Part_1/courseinfo/src/App.jsx
@@ -9,17 +9,11 @@ const App = () => {
 
   return (
     <>
-      <Part name = {part1} exercises = {exercises1}/>
-      <Part name = {part2} exercises = {exercises2}/>
-      <Part name = {part3} exercises = {exercises3}/>
-    </>
-  )
-}
-
-const Part = ({name, exercises}) => {
-  return(
-    <>
-      <p> {name} {exercises}</p>
+      <Header course = {course}/>
+      <Content part1 = {part1} exercises1 = {exercises1}/>
+      <Content part2 = {part2} exercises2 = {exercises2}/>
+      <Content part3 = {part3} exercises3 = {exercises3}/>
+      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
     </>
   )
 }
@@ -35,9 +29,9 @@ const Header = (props) => {
 const Content = (props) => {
   return(
     <>
-      <p> {props.part1} {props.exercises1} </p>
-      <p> {props.part2} {props.exercises2} </p>
-      <p> {props.part3} {props.exercises3} </p>
+    <Part part = {props.part1} exercises = {props.exercises1}/>
+    <Part part = {props.part2} exercises = {props.exercises2}/>
+    <Part part = {props.part3} exercises = {props.exercises3}/>
     </>
   )
 }
@@ -46,6 +40,14 @@ const Total = (props) => {
   return(
     <>
       <p> Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <>
+      <p> {props.part} {props.exercises}</p>
     </>
   )
 }
